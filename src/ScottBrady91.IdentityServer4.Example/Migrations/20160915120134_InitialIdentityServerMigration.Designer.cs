ab7d6c4e39fd1b5f3708dbe4eb1acd1639d3d872
Scott Brady <scott.brady@rocksolidknowledge.com>
2016-09-15 13:59:53 +0100

    Fixed ASP.NET Identity registration and added test data ef migrations

---
+++ src/ScottBrady91.IdentityServer4.Example/Migrations/20160915120134_InitialIdentityServerMigration.Designer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using IdentityServer4.EntityFramework.DbContexts;
+
+namespace ScottBrady91.IdentityServer4.Example.Migrations
+{
+    [DbContext(typeof(PersistedGrantDbContext))]
+    [Migration("20160915120134_InitialIdentityServerMigration")]
+    partial class InitialIdentityServerMigration
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.0.1")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("IdentityServer4.EntityFramework.Entities.PersistedGrant", b =>
+                {
+                    b.Property<string>("Key");
+
+                    b.Property<string>("Type");
+
+                    b.Property<string>("ClientId")
+                        .IsRequired()
+                        .HasAnnotation("MaxLength", 200);
+
+                    b.Property<DateTime>("CreationTime");
+
+                    b.Property<string>("Data")
+                        .IsRequired();
+
+                    b.Property<DateTime>("Expiration");
+
+                    b.Property<string>("SubjectId");
+
+                    b.HasKey("Key", "Type");
+
+                    b.ToTable("PersistedGrants");
+                });
+        }
+    }
+}
--- src/ScottBrady91.IdentityServer4.Example/Startup.cs
+++ src/ScottBrady91.IdentityServer4.Example/Startup.cs
@@ -5,7 +5,10 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using ScottBrady91.IdentityServer4.Example.Configuration;
 using System.Reflection;
+using IdentityServer4.EntityFramework.DbContexts;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System.Linq;
+using IdentityServer4.EntityFramework.Mappers;
 
 namespace ScottBrady91.IdentityServer4.Example
 {
@@ -13,7 +16,8 @@ namespace ScottBrady91.IdentityServer4.Example
     {
         public void ConfigureServices(IServiceCollection services)
         {
-            const string connectionString = @"Data Source=(LocalDb)\MSSQLLocalDB;database=Test.IdentityServer4.EntityFramework;trusted_connection=yes;";
+            const string connectionString =
+                @"Data Source=(LocalDb)\MSSQLLocalDB;database=ScottBrady91.IdentityServer4.Example;trusted_connection=yes;";
             var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
 
             // ASP.NET Identity DbContext
@@ -44,10 +48,69 @@ namespace ScottBrady91.IdentityServer4.Example
 
             app.UseDeveloperExceptionPage();
 
+            InitializeDbTestData(app);
+
+            app.UseIdentity();
             app.UseIdentityServer();
 
             app.UseStaticFiles();
             app.UseMvcWithDefaultRoute();
+        }
+
+        private static void InitializeDbTestData(IApplicationBuilder app)
+        {
+            using (var scope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                scope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+                scope.ServiceProvider.GetRequiredService<ConfigurationDbContext>().Database.Migrate();
+                scope.ServiceProvider.GetRequiredService<ApplicationDbContext>().Database.Migrate();
+
+                var context = scope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+                
+                if (!context.Clients.Any())
+                {
+                    foreach (var client in Clients.Get())
+                    {
+                        context.Clients.Add(client.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.Scopes.Any())
+                {
+                    foreach (var client in Scopes.Get())
+                    {
+                        context.Scopes.Add(client.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                var identityDbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                if (!identityDbContext.Users.Any())
+                {
+                    foreach (var inMemoryUser in Users.Get())
+                    {
+                        var identityUser = new IdentityUser(inMemoryUser.Username)
+                        {
+                            Id = inMemoryUser.Subject
+                        };
+
+                        foreach (var claim in inMemoryUser.Claims)
+                        {
+                            identityUser.Claims.Add(new IdentityUserClaim<string>
+                            {
+                                UserId = identityUser.Id,
+                                ClaimType = claim.Type,
+                                ClaimValue = claim.Value
+                            });
+                        }
+
+                        identityDbContext.Users.Add(identityUser);
+                    }
+
+                    identityDbContext.SaveChanges();
+                }
+            }
         }
     }
 }
